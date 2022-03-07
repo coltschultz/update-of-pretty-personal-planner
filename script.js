c3aae2b3afc5b580259ecefaef44bfcae3606fb7
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-06 18:36:45 -0600

    set up divs

---
+++ script.js
@@ -0,0 +1,23 @@
+var dateNow = moment();
+
+// Get the hour (1-24)
+var thisHour = moment().hour();
+var thisDay = moment().dayOfYear();
+console.log('dateNow', dateNow);
+console.log('thisHour', thisHour);
+console.log('thisDay', thisDay)
+// var timeNow =
+
+// Determine the current date
+// Determine if there's already localstorage for this date, if so load, if not, continue:
+// Setup object array for that day to store in localstorage under the date
+// Display the date at the top of the calender 
+
+// Determine the current time
+// Color code time blocks according to past/present/future hour 9AM-5AM
+// Print timeblocks to page
+
+// When you click an hour, you can edit the hour
+// When you an hour blurs, save that hour and update localstorage
+
+// When the SaveBtn is clicked, is anything necessary?
