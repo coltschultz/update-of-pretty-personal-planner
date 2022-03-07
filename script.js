9db20bd291356bf93064f3aab3d6904f145c1e85
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-06 21:05:19 -0600

    begin adding event listeners

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
 
-var hours = [nine, ten, eleven, twelve, one, two, three, four, five];
+var hours = [nine, ten, eleven, twelve];
 
-var nine = "";
-var ten = "";
-var eleven = "";
-var twelve = "";
-var one = "";
-var two = "";
-var three = "";
-var four = "";
-var five = "";
+var nine = document.querySelector('#nineContent');
+var ten = document.querySelector('#tenContent');
+var eleven = document.querySelector('#elevenContent');
+var twelve = document.querySelector('#twelveContent');
+var one = document.querySelector('#oneContent');
+var two = document.querySelector('#twoContent');
+var three = document.querySelector('#threeContent');
+var four = document.querySelector('#fourContent');
+var five = document.querySelector('#fiveContent');
 
-
-// Determine the current date
-var dateNow = moment();
-
-// Get the hour (1-24)
 var thisHour = moment().hour();
+var dateNow = moment();
+var thisDay = moment().dayOfYear();
+
+var togglePast = function(hour) {
+    $(hour).removeClass("present");
+    $(hour).addClass("past");
+}
+
+var toggleFuture = function(hour) {
+    $(hour).removeClass("present");
+    $(hour).addClass("future");
+}
+
+var checkTime = function(hour) {
+    console.log('testing' + hour);
+    if (thisHour > hour.dataset.time) { 
+        togglePast(hour);
+        console.log('in the past')
+    }
+    else if (thisHour < hour.dataset.time) {
+        toggleFuture(hour);
+    }
+}
+
+var editContent = function(Time) {
+    var contentSelected = document.querySelector(".content[data-time='" + Time + "']");
+
+    var content = contentSelected.querySelector("p").textContent;
+
+// task text was clicked
+$(".text").on("click", "p", function() {
+    // get current text of p element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+  });
+
+checkTime(nine);
+checkTime(ten);
+checkTime(eleven);
+checkTime(twelve);
+console.log('test', thisHour + eleven.dataset.time);
+
 
 // Determine if there's already localstorage for this date, if so load, if not, continue:
 // Setup object array for that day to store in localstorage under the date
-var thisDay = moment().dayOfYear();
+
 
 
 
