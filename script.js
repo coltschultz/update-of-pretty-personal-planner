3d7ae5213340a1a9cd67a8343f6155081f7b6021
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-06 19:00:05 -0600

    add more hours and some initial variables

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+
+var hours = [nine, ten, eleven, twelve, one, two, three, four, five];
+
+var nine = "";
+var ten = "";
+var eleven = "";
+var twelve = "";
+var one = "";
+var two = "";
+var three = "";
+var four = "";
+var five = "";
+
+
+// Determine the current date
 var dateNow = moment();
 
 // Get the hour (1-24)
 var thisHour = moment().hour();
+
+// Determine if there's already localstorage for this date, if so load, if not, continue:
+// Setup object array for that day to store in localstorage under the date
 var thisDay = moment().dayOfYear();
+
+
+
 console.log('dateNow', dateNow);
 console.log('thisHour', thisHour);
 console.log('thisDay', thisDay)
-// var timeNow =
 
-// Determine the current date
-// Determine if there's already localstorage for this date, if so load, if not, continue:
-// Setup object array for that day to store in localstorage under the date
-// Display the date at the top of the calender 
+
+
+
+// Display the date at the top of the calender
 
 // Determine the current time
 // Color code time blocks according to past/present/future hour 9AM-5AM
