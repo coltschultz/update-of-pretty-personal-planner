9ee1f16348ab0b196f2f676e6a4d75c6782b2a5e
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-06 21:41:18 -0600

    add date

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var hours = [nine, ten, eleven, twelve];
 
+var dateBox = document.querySelector('#date');
 var nine = document.querySelector('#nineContent');
 var ten = document.querySelector('#tenContent');
 var eleven = document.querySelector('#elevenContent');
@@ -12,8 +13,16 @@ var four = document.querySelector('#fourContent');
 var five = document.querySelector('#fiveContent');
 
 var thisHour = moment().hour();
-var dateNow = moment();
+var year = moment().year()
+var month = moment().month()
+var day = moment().date()
 var thisDay = moment().dayOfYear();
+
+dateBox.innerHTML = "<p>" + month + "-" + day + "-" + year + "</p>"
+    // var shownDate = document.createElement("p");
+    // shownDate.textContent = "asdf" + thisDay;
+    // dateBox.appendChild(showdate);
+
 
 var togglePast = function(hour) {
     $(hour).removeClass("present");
@@ -26,10 +35,8 @@ var toggleFuture = function(hour) {
 }
 
 var checkTime = function(hour) {
-    console.log('testing' + hour);
     if (thisHour > hour.dataset.time) { 
         togglePast(hour);
-        console.log('in the past')
     }
     else if (thisHour < hour.dataset.time) {
         toggleFuture(hour);
@@ -40,27 +47,43 @@ var editContent = function(Time) {
     var contentSelected = document.querySelector(".content[data-time='" + Time + "']");
 
     var content = contentSelected.querySelector("p").textContent;
+}
 
-// task text was clicked
-$(".text").on("click", "p", function() {
+
+// Handle the clicks of content
+$(".content").on("click", "p", function() {
+    console.log('success');
     // get current text of p element
     var text = $(this)
       .text()
       .trim();
   
     // replace p element with a new textarea
-    var textInput = $("<textarea>").addClass("form-control").val(text);
+    var textInput = $("<textarea>").val(text);
     $(this).replaceWith(textInput);
   
     // auto focus new element
     textInput.trigger("focus");
-  });
+  }); 
+
+  // editable field was un-focused
+$(".content").on("blur", "textarea", function() {
+    // get current value of textarea
+    var text = $(this).val();
+  
+
+      var textP = $("<p>")
+      .addClass("m-1")
+      .text(text);
+  
+    // replace textarea with new content
+    $(this).replaceWith(textP);
+});
 
 checkTime(nine);
 checkTime(ten);
 checkTime(eleven);
 checkTime(twelve);
-console.log('test', thisHour + eleven.dataset.time);
 
 
 // Determine if there's already localstorage for this date, if so load, if not, continue:
@@ -85,4 +108,6 @@ console.log('thisDay', thisDay)
 // When you click an hour, you can edit the hour
 // When you an hour blurs, save that hour and update localstorage
 
-// When the SaveBtn is clicked, is anything necessary?
+// When the SaveBtn is clicked, is anything necessary
+
+// Date is 1 month behind
